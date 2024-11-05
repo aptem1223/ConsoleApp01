d9edfb5f5dd977ab03af0622510d6afcd9cd51ea
Аня <@IDEAPAD>
2024-11-05 02:01:48 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp01/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Random rand = new Random();
+            //int[] array = new int[15];
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    array[i] = rand.Next(1, 101);  
+            //}
+            //Console.WriteLine("Массив случайных чисел:");
+            //foreach (int num in array)
+            //{
+            //    Console.Write(num + " ");
+            //}
+            //Console.WriteLine();
+
+            //Console.WriteLine("Числа, кратные 11:");
+            //bool found = false;  
+            //foreach (int num in array)
+            //{
+            //    if (num % 11 == 0)  
+            //    {
+            //        Console.Write(num + " ");
+            //        found = true;
+            //    }
+            //}
+            //if (!found)
+            //{
+            //    Console.WriteLine("В массиве нет чисел, кратных 11.");
+            //}
+            //
+
+
+            //int[] array = { 12, -9, 6, -3, 9, 0, -8, 15, -1, 4 };
+            //int sum = 0;
+            //int count = 0;
+            //foreach (int num in array)
+            //{
+            //    if (num > 0)  
+            //    {
+            //        sum += num;  
+            //        count++;      
+            //    }
+            //}
+            //Console.WriteLine($"Сумма положительных элементов: {sum}");
+            //Console.WriteLine($"Количество положительных элементов: {count}");
+            //Console.ReadLine();
+
+            //Console.Write("Введите количество элементов в массиве: ");
+            //int n = int.Parse(Console.ReadLine());
+            //int[] array = new int[n];
+            //Console.WriteLine("Введите элементы массива:");
+            //for (int i = 0; i < n; i++)
+            //{
+            //    Console.Write($"Элемент {i + 1}: ");
+            //    array[i] = int.Parse(Console.ReadLine());
+            //}
+            //Console.WriteLine("Элементы массива в обратном порядке:");
+            //for (int i = n - 1; i >= 0; i--)
+            //{
+            //    Console.Write(array[i] + " ");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
